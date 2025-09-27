4354c7f966127c2756b5df2e411fbdbe2fe34b2a
Bellal-SE <bilal.kanjo184@gmail.com>
2025-09-27 17:15:22 +0500

    Add bg-changer

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+
+function App() {
+  const colors = [
+    { name: "Red", value: "#FF0000" },
+    { name: "Blue", value: "#0000FF" },
+    { name: "Yellow", value: "#FFFF00" },
+    { name: "Green", value: "#00FF00" },
+    { name: "Purple", value: "#800080" },
+];
+ const [color,setColor] = useState("green")
+  return (
+    <>
+    <div className="duration-300 w-full h-screen"
+    style={{backgroundColor : color}}
+    >
+      <div className="flex flex-wrap fixed bottom-16 inset-x-0  p-2  justify-center  rounded-2xl">
+        <div className="flex flex-wrap justify-center gap-3 bg-gray-400 p-2 rounded-2xl">
+         {colors.map((c) => (
+          <button
+          key = {c.value}
+          className = "text-white rounded-2xl px-4 py-2 cursor-pointer shadow-lg capitalize"
+          onClick={() => setColor(c.value)}
+          style={{backgroundColor : c.value}}
+          >{c.name}</button> 
+         ))}
+          
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
